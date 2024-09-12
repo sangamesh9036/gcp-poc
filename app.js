ffb8b58d8d9f5979c58fad36a72060efbe0e9196
sangamesh9036 <sangameshrb06@gmail.com>
2024-09-12 10:34:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('HELLO-GCP-POC-new.');
+    res.send('HELLO-GCP-POC.');
 });
 
 const port = process.env.PORT || 3000;
